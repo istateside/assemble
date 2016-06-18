bb1068d2809f23ded96518a1d6a5cd0289bf346f
Liz Baillie <lbaillie@simplereach.com>
2016-06-17 23:26:37 -0700

    adds mirage + fake users

---
+++ mirage/factories/user.js
@@ -0,0 +1,10 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  username() {
+    return faker.internet.userName();
+  },
+  onCall() {
+    return faker.random.boolean();
+  }
+});
