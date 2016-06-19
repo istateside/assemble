f26f465807ac5413e649543b0d9617d4437d1d92
Liz Baillie <lbaillie@simplereach.com>
2016-06-18 22:23:42 -0700

    adds geolocation addon + index controller

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  geolocation: Ember.inject.service(),
+
   model() {
     return this.store.findAll('user');
+  },
+
+  actions: {
+    getUserLocation() {
+      this.get('geolocation').getLocation().then(() => {
+        var currentLocation = this.get('geolocation').get('currentLocation');
+        this.controllerFor('index').set('userLocation', currentLocation);
+      });
+    }
   }
 });
