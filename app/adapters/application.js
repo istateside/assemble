a285d39d07dbdb3f5923bdfccfded6e5f316c1b8
Liz Baillie <liz@lizbaillie.com>
2016-11-15 08:43:43 -0800

    adds active model adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,5 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.JSONAPIAdapter.extend({
+export default ActiveModelAdapter.extend({
+  host: 'http://localhost:3000'
 });
