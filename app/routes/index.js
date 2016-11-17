a8a9976b808aa0fbf859abe432866e064f8fee98
Liz Baillie <liz@lizbaillie.com>
2016-11-16 16:31:06 -0800

    updates ember-cli-geo + basic implementation of current user location

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  geolocation: Ember.inject.service(),
+
   model: function() {
     return this.store.findAll('user');
+  },
+
+  actions: {
+    getCurrentUserLocation() {
+      this.get('geolocation').getLocation().then((geoObject) => {
+        this.controllerFor('index').set('currentUserLocation',
+          geoObject.coords.latitude + ", " + geoObject.coords.longitude);
+      });
+    }
   }
 });
