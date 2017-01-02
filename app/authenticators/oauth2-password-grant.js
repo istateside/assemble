68035401c65bde65db7e72b0856cc2c091c0d296
Liz Baillie <liz@lizbaillie.com>
2017-01-02 14:32:59 -0800

    Remove currently unused OAuth authenticator

--- app/authenticators/oauth2-password-grant.js
+++
@@ -1,5 +0,0 @@
-import OAuth2 from 'ember-simple-auth/authenticators/oauth2-password-grant';
-
-export default OAuth2.extend({
-  //serverTokenEndpoint: 'http://localhost:3000/auth/google_oauth2'
-});
