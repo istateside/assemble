372207b674be663bbcba8d6f57221401174e7928
L. Baillie <liz@lizbaillie.com>
2017-01-07 14:38:34 -0800

    Make currentUser work for really realz

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  session: Ember.inject.service(),
+
+  beforeModel() {
+    return this._loadCurrentUser();
+  },
+
+  sessionAuthenticated() {
+    this._loadCurrentUser().then(() => {
+      this.transitionTo('/');
+    }).catch(() => this.get('session').invalidate());
+  },
+
+  _loadCurrentUser() {
+    this.get('session').loadCurrentUser();
+  }
+});
--- app/services/session.js
+++ app/services/session.js
@@ -2,14 +2,18 @@ import DS from 'ember-data';
 import Session from "ember-simple-auth/services/session";
 
 export default Session.extend({
-
   store: Ember.inject.service(),
 
-  currentUser: Ember.computed('isAuthenticated', function() {
-    if (this.get('isAuthenticated')) {
-      const promise = this.get('store').queryRecord('user', { current_user: 'true' });
-      return DS.PromiseObject.create({ promise: promise });
-    }
-  })
-
+  loadCurrentUser() {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      if (this.get('isAuthenticated')) {
+        return this.get('store').findRecord('user', 'me').then((user) => {
+          this.set('currentUser', user);
+          resolve();
+        }, reject);
+      } else {
+        resolve();
+      }
+    });
+  }
 });
