7ec25bc6fe376d2a1d3fe5710489c84b307c5746
L. Baillie <liz@lizbaillie.com>
2017-01-08 18:33:31 -0800

    Add `session` documentation

--- app/services/session.js
+++ app/services/session.js
@@ -1,9 +1,23 @@
 import DS from 'ember-data';
 import Session from "ember-simple-auth/services/session";
 
+/**
+  A service extending Ember Simple Auth's provided `session` service.
+  Used primarily for providing information on the current user.
+  @class service:session
+  @extends ember-simple-auth/services/session
+  @type Ember.Service
+*/
+
 export default Session.extend({
   store: Ember.inject.service(),
 
+  /**
+    A method to get and set the current user's data if authenticated.
+
+    @public
+    @method loadCurrentUserData
+  */
   loadCurrentUserData() {
     return new Ember.RSVP.Promise((resolve, reject) => {
       if (this.get('isAuthenticated')) {
