bb221d9a09aae23fcb4bbcc80d3102f3f1da0cbd
L. Baillie <liz@lizbaillie.com>
2017-01-24 08:49:22 -0800

    First pass at creation of Google Map service

---
+++ app/services/google-map.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  googleMaps: window.google.maps,
+
+  // this whole thing is from the Google Maps API geolocation example
+  initMap() {
+      let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 20
+      });
+
+      let infoWindow = new google.maps.InfoWindow({map: map});
+
+      // try HTML5 geolocation
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+  },
+
+  handleLocationError(browserHasGeolocation, infoWindow, post) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: the geolocation service failed' : "Error: your browser doesn't support geolocation");
+  }
+});
